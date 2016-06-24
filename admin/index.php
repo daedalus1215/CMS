6335168cdef845578ebc95bc2e37eddd1f0d0b26
Larry Adams <ladams1776@yahoo.com>
2016-06-23 22:44:02 -0400

    restruct

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <?php include("includes/top_nav.php"); ?>
+            <?php include("includes/side_nav.php"); ?>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <?php include('includes/admin_content.php'); ?>
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>

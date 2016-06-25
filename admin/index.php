ceefe789168dc9a903ffcbd63f22233003cf3f06
Larry <ladams1776@yahoo.com>
2016-06-25 10:06:04 -0400

    refactored user class

--- admin/index.php
+++ admin/index.php
@@ -24,14 +24,46 @@
 
 
 
-    $users = User::find_all_users();
-    foreach ($users as $user) {
-        echo $user['first_name'] . "<br/>";
-    }
+    $found_user = User::find_user_by_id(1);
 
-    $user = User::find_user_by_id(1);
-    echo $user['first_name'];
+    $user               = new User();
+    $user->id           = $found_user['id'];
+    $user->username     = $found_user['username'] ;
+    $user->password     = $found_user['password'];
+    $user->first_name   = $found_user['first_name'];
+    $user->last_name    = $found_user['last_name'];
+
+    echo $user->username;
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                         <ol class="breadcrumb">
                             <li>

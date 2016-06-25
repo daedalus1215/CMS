25c9c23ad6a965650890883056dc9bcf3d75f964
Larry <ladams1776@yahoo.com>
2016-06-25 15:11:36 -0400

    Building up our methods to build up and deliver a User object

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -46,20 +46,33 @@ class User
         return $result_set;
     }
 
-
-    public static function instantiation($found_user)
+    /**
+     *
+     * @param Array $the_record is a returned record, a array of the row.
+     * @return \self
+     */
+    public static function instantiation($the_record)
     {
         $the_object               = new self();
 
-        $the_object->id           = $found_user['id'];
-        $the_object->username     = $found_user['username'] ;
-        $the_object->password     = $found_user['password'];
-        $the_object->first_name   = $found_user['first_name'];
-        $the_object->last_name    = $found_user['last_name'];
+        foreach ($the_record as $the_attribute => $value) {
+            if ($the_object->has_the_attribute($the_attribute)) {
+                $the_object->the_attribute = $value;
+            }
+        }
+
 
         return $the_object;
     }
 
-
+    /**
+     * Check to see if this String matches the object's attribute or property
+     * @param String Name of a property this object should have.
+     */
+    private function has_the_attribute($the_attribute)
+    {
+        $object_properties = get_object_vars($this);
+        array_key_exists($the_attribute, $object_properties);
+    }
 }
 

d329080bedef08c936a6f12d588f061417c13fb7
ladams1776 <ladams1776@yahoo.com>
2016-07-09 15:21:23 -0400

    building up comments.php

--- admin/comments.php
+++ admin/comments.php
@@ -1,4 +1,15 @@
 <?php include("includes/header.php"); ?>
+<?php if(!$session->is_signed_in()) {redirect("login.php"); }?>
+
+<?php 
+
+$comments = Comment::find_all();
+
+?>
+
+
+
+
 
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -15,9 +26,11 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            COMMENTS
+                            USERS
                             <small>Subheading</small>
                         </h1>
+                        <a href="add_comment.php">Add comment</a>
+                        
                         <ol class="breadcrumb">
                             <li>
                                 <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
@@ -25,7 +38,45 @@
                             <li class="active">
                                 <i class="fa fa-file"></i> Blank Page
                             </li>
-                        </ol>
+                        </ol>                                              
+                        
+                        <div class="col-md-12">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Photo's Id</th>
+                                        <th>Author</th>
+                                        <th>Body</th>                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($comments as $comment):?>
+                                    <tr>                                                                                                                       
+                                        <td>
+                                            <?php echo $comment->id; ?>
+                                            <!-- Control of each one. -->
+                                            <div class="picture_link">
+                                                <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>
+                                                <a href="edit_comment.php?id=<?php echo $comment->id; ?>">Edit</a>
+                                                <a href="view_comment.php?id=<?php echo $comment->id; ?>">View</a>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <?php echo $comment->photo_id?>
+                                        </td>
+                                        <td>
+                                            <?php echo $comment->author; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $comment->body; ?>
+                                        </td>                                        
+                                    </tr>                                       
+                                    <?php endforeach; ?>    
+                                       
+                                </tbody>
+                            </table>                                                                                   
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->

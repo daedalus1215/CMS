f85d3463f07d6be94b23978277aaa954aac39b7f
ladams1776 <ladams1776@yahoo.com>
2016-07-09 23:18:45 -0400

    users and comments now wired to dashboard

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -71,10 +71,7 @@
                                         <i class="fa fa-user fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">8
-
-                                        </div>
-
+                                        <div class="huge"><?php echo User::count_all(); ?></div>
                                         <div>Users</div>
                                     </div>
                                 </div>
@@ -97,7 +94,7 @@
                                         <i class="fa fa-support fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">8</div>
+                                        <div class="huge"><?php echo Comment::count_all(); ?></div>
                                         <div>Comments</div>
                                     </div>
                                 </div>

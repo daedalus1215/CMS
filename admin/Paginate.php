bafbfd650804f9b238bae49d0014141ea7e13ed8
ladams1776 <ladams1776@yahoo.com>
2016-07-10 09:15:58 -0400

    constructor done

--- admin/Paginate.php
+++ admin/Paginate.php
@@ -14,15 +14,23 @@
 class Paginate 
 {
     // The page we are on.
-    public $page;
+    public $current_page;
     // Essentially our limit.
     public $items_per_page;
     // The total.
     public $items_total_count;
     
-    public function __construct() 
+    /**
+     * 
+     * @param int $page - The page we are on.
+     * @param int $items_per_page - Essentially our limit.
+     * @param int $items_total_count - The total.
+     */
+    public function __construct($page=1, $items_per_page=4, $items_total_count=0) 
     {
-        
+       $this->current_page   = (int) $page;
+       $this->items_per_page = (int) $items_per_page;
+       $this->items_total_count    = (int) $items_total_count;
     }
     
     

e984186282bf8440ebc03d38029950d3dc1d9ac4
ladams1776 <ladams1776@yahoo.com>
2016-07-09 23:35:21 -0400

    beginning to work on index.php

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php include("includes/header.php"); ?>
 
+    
+    
+<?php $photos = Photo::find_all(); ?>
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
+            <div class="col-md-12">
+                <?php foreach($photos as $photo): ?>
+                
+                <div class="thumbnails row">
+                    <div class="col-xs-6 col-md-3">
+                        <a class="thumbnail" href="">
+                            <img src="" alt="">
+                        </a>
+                    </div>
+                </div>
+                
+                
+                
+                <?php endforeach; ?>
             </div>
 
 
@@ -18,13 +28,8 @@
 
             <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
-
-            
                  <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
+            </div>
         <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>

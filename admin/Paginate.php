4f62cd336fcb01a353c652b6bd261f9e2818ae1f
ladams1776 <ladams1776@yahoo.com>
2016-07-10 09:14:20 -0400

    starting the pagination class

---
+++ admin/Paginate.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This will handle all our paginations.
+ *
+ * @author larry
+ */
+class Paginate 
+{
+    // The page we are on.
+    public $page;
+    // Essentially our limit.
+    public $items_per_page;
+    // The total.
+    public $items_total_count;
+    
+    public function __construct() 
+    {
+        
+    }
+    
+    
+    
+}

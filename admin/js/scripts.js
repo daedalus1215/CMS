2a39b6162891b48a876ae5d81c4ebe70babb78bf
ladams1776 <ladams1776@yahoo.com>
2016-07-11 21:05:38 -0400

    grabbing the image id as well as the userid

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -8,17 +8,24 @@ $(document).ready(function(){
         var user_href;
         var user_href_splitted;
         var userId;
+        var image_src;
+        var image_href_splitted;
+        var image_id;
         
         // Make sure the button is now selectable.
         $('#set_user_image').prop('disabled', false);
         
-         user_href = $('#user-id').prop('href');
+        // from user-id anchor tag on edit_user.php
+        user_href = $('#user-id').prop('href'); // looks like http://.../..?id=8         
+        user_href_splitted = user_href.split('='); // lets split on that '='        
+        userId = user_href_splitted[user_href_splitted.length - 1]; // lets grab that 8.
          
-         user_href_splitted = user_href.split('=');
          
-         userId = user_href_splitted[user_href_splitted.length - 1];
-         
-         alert(userId);
+        image_src = $(this).prop("src"); //grabbing the images path and name includes/images/image_name.jpg
+        image_href_splitted = image_src.split("/");  
+        image_id = image_href_splitted[image_href_splitted.length -1]; // grab that image_name.jpg
+        
+        alert(image_id);
         
         
         

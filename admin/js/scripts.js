15248c60f700839532c998ced7e2002da5543e58
ladams1776 <ladams1776@yahoo.com>
2016-07-11 20:54:50 -0400

    grabbing user's id from the anchor of edit_photo

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -5,8 +5,20 @@
  */
 $(document).ready(function(){
     $('.modal_thumbnails').click(function() {
+        var user_href;
+        var user_href_splitted;
+        var userId;
+        
         // Make sure the button is now selectable.
         $('#set_user_image').prop('disabled', false);
+        
+         user_href = $('#user-id').prop('href');
+         
+         user_href_splitted = user_href.split('=');
+         
+         userId = user_href_splitted[user_href_splitted.length - 1];
+         
+         alert(userId);
         
         
         
